ace061f0b2002100ea71cdec3eb61b5f00523f28
AKROMADOKSA\idiaz <idiazmiragaya294@gmail.com>
2025-03-19 00:39:35 +0100

    more testing

--- sql/database.php
+++ sql/database.php
@@ -1,3 +1,16 @@
 <?php
-phpinfo();
+$servername = "PMYSQL185.dns-servicio.com:3306"; // o la IP de tu servidor
+$username = "zagg294"; // Tu nombre de usuario de la base de datos
+$password = "Ivan1246354."; // Tu contraseña de la base de datos
+$dbname = "10853313_general"; // El nombre de tu base de datos
+
+// Crear la conexión
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if (!$conn) {
+    die("Conexión fallida: " . mysqli_connect_error());
+}
+
+echo "Conexión exitosa!";
 ?>
